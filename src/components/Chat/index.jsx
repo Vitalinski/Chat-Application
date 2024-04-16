7353b8559d76bce2bb70582019bf33e00f0e0624
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2024-04-16 12:52:07 +0200

    Added top field in Chat section

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -2,7 +2,27 @@ import styles from  "./Chat.module.scss"
 
 const Chat = () => {
   return (
-    <div className={styles.chat}>Chat</div>
+    <div className={styles.chat}>
+      <div className={styles.top}>
+      <div className={styles.top__user}>
+<img src="./avatar.png" alt="" />
+<div className={styles.top__texts}>
+<span>Sample Friend</span>
+<p>Sample user description</p>
+</div>
+
+      </div>
+   <div className={styles.top__icons}>
+   <img src="./phone.png" alt="" />
+   <img src="./video.png" alt="" />
+   <img src="./info.png" alt="" />
+
+      </div>
+      </div>
+      <div className={styles.center}></div>
+      <div className={styles.bottom}></div>
+
+    </div>
   )
 }
 
