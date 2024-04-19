c7732bfd3478e53ee756f60a62abd90716805b13
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2024-04-19 19:13:17 +0200

    Added Firebase login and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import Chat from "./components/Chat";
 import Detail from "./components/Detail";
 import List from "./components/List";
 import Login from "./components/Login";
 import Notification from "./components/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 const App = () => {
-  let user = true;
+  let user = false;
+  useEffect(()=>{
+    const onSub = onAuthStateChanged(auth, (user)=>{
+console.log(user)
+    })
+    return ()=> onSub()
+  },[])
   return (
     <div className="container">
       {user ? (
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,23 +1,63 @@
 import { useState } from "react";
 import styles from "./Login.module.scss";
 import { toast } from "react-toastify";
-
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../lib/firebase.js";
+import { doc, setDoc } from "firebase/firestore";
+import upload from "../../lib/unload.js";
 const Login = () => {
   const [avatar, setAvatar] = useState({
     file: null,
     url: "",
   });
+  const [loading, setLoading] = useState(false);
   const handleAvatar = (e) => {
-    setAvatar({
-      file: e.target.files[0],
-      url: URL.createObjectURL(e.target.files[0]),
-    });
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
   };
-  const handleLogin = (e)=>{
-    e.preventDefault()
-    toast.success('Hello')
 
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const {  email, password } = Object.fromEntries(formData);
+    try {
+     await signInWithEmailAndPassword(auth,email, password)
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const imgUrl = await upload(avatar.file);
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        avatar: imgUrl,
+        id: res.user.uid,
+        blocked: [],
+      });
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: [],
+      });
+      toast.success("Account created! You can login now!");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className={styles.login}>
       <div className={styles.item}>
@@ -25,14 +65,16 @@ const Login = () => {
         <form onSubmit={handleLogin}>
           <input type="text" placeholder="Email" name="email" />
           <input type="text" placeholder="Password" name="password" />
-          <button>Sign In </button>
+          <button disabled={loading}>
+            {loading ? "Loading..." : "Sign In"}{" "}
+          </button>
         </form>
       </div>
 
       <div className={styles.separator}></div>
       <div className={styles.item}>
         <h2>Create Account</h2>
-        <form>
+        <form onSubmit={handleRegister}>
           <label htmlFor="file">
             <img src={avatar.url || "./avatar.png"} alt="" />
             Upload your avatar
@@ -47,7 +89,9 @@ const Login = () => {
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="text" placeholder="Password" name="password" />
-          <button>Sign Up </button>
+          <button disabled={loading}>
+            {loading ? "Loading..." : "Sign Up"}{" "}
+          </button>
         </form>
       </div>
     </div>
